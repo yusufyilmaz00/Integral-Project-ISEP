b36d9abe40450a98cb2b6cc8e59a570b9398d81e
Miguff <mihgeza2000@gmail.com>
2024-05-02 02:34:34 +0200

    Created a Universal way for the US13 data path

--- src/main/java/sprintB/us12/Main.java
+++ src/main/java/sprintB/us12/Main.java
@@ -1,12 +1,18 @@
 package sprintB.us12;
 
+import java.io.File;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
+        //Get the two FilePath, that we will need for US13
+        String FilePathDos = System.getProperty("user.dir") + "\\docs\\sprintB\\us13\\Data\\US13_JardimDosSentimentos.csv";
+        String FilePathEspecies = System.getProperty("user.dir") + "\\docs\\sprintB\\us13\\Data\\US13_JardimEspeciesNucleoRural.csv";
+
+        //Creates the Routes, now only just one, later we will decide how to implement.
         Routes r = new Routes();
-        r.importFromCsv("C:\\Users\\jerem\\OneDrive - bwedu\\Studium\\7tes Semester\\PPROG\\Allgemein\\Sprint_2\\Data\\US13_JardimEspeciesNucleoRural.csv"); // Provide the correct file path here
+        r.importFromCsv(FilePathEspecies); // Provide the correct file path here
 
         System.out.println(r);
 
