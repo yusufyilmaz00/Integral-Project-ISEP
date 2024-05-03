86b61fa1b7a8887c4259159da045353ef58ca82d
Miguff <mihgeza2000@gmail.com>
2024-05-03 13:08:05 +0200

    Added the class diagram and the svg file, plus started the javadoc

--- src/main/java/sprintB/us01/Collaborator.java
+++ src/main/java/sprintB/us01/Collaborator.java
@@ -10,6 +10,13 @@ public class Collaborator {
     private String phone;
     private List<Skill> skills;
 
+    /**
+     * This creates or constructs the collaborator, it requires a name, an email address and a phone number
+     * @param name The name of the collaborator
+     * @param email The email of the collaborator
+     * @param phone The phone number of the collaborator
+     */
+
     public Collaborator(String name, String email, String phone) {
         this.name = name;
         this.email = email;
@@ -17,22 +24,45 @@ public class Collaborator {
         this.skills = new ArrayList<>();
     }
 
+    /**
+     * Gets the name of the collaborator
+     * @return retunrs the name of the collaborator
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * Gets the email of the collaborator
+     *
+     * @return returns the email of the collaborator
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Gets the phone numbe rof the collaborator
+     *
+     * @return return the phone number
+     */
     public String getPhone() {
         return phone;
     }
 
+    /**
+     * Here we can add skill to a collaborator
+     * @param skill this a skill, which has to be one from the Skill class
+     */
     public void addSkill(Skill skill){
         skills.add(skill);
     }
 
+    /**
+     * If we want to know what skills our collaborator can do, we can return it
+     * @return returns the list of the skill that the collaborator can do
+     */
     public List<Skill> getSkills(){
         return skills;
     }
--- src/main/java/sprintB/us01/Skill.java
+++ src/main/java/sprintB/us01/Skill.java
@@ -3,14 +3,29 @@ package sprintB.us01;
 public class Skill {
     private String name;
 
+    /**
+     * Create a skill with a name
+     *
+     * @param name this is the name of the skill
+     */
     public Skill(String name) {
         this.name = name;
     }
 
+    /**
+     * It gets the name of the Skill when we want to print out for example what skill, does our collaborator has
+     *
+     * @return return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * If we want to change the name of the skill, because we mistyped we can do it here,
+     *
+     * @param name is the new name of the skill
+     */
     public void setName(String name) {
         this.name = name;
     }
