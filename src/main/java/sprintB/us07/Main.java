77822fc6afb992cddfc2e2d417e5211a61a2ab0b
DragomirMarinov <drago090802@gmail.com>
2024-05-12 20:16:36 +0100

    vehicle  class update

--- src/main/java/sprintB/us07/Main.java
+++ src/main/java/sprintB/us07/Main.java
@@ -3,13 +3,10 @@ package sprintB.us07;
 import java.util.Date;
 public class Main {
     public static void main(String[] args) {
-// Creating a vehicle
-        Vehicle vehicle = new Vehicle("Toyota", "Corolla", "Sedan", 1200, 1500,
-                50000, new Date(), new Date(), 10000);
 
-        // Registering maintenance for the vehicle
-        Maintenance maintenance = new Maintenance(vehicle, new Date(), 51000);
+//Creating Vehicle
 
+       // Vehicle vehicle = new Vehicle("Mercedes","S class", "Hedge","123",2,6,5555,)
 
     }
 }
---
+++ src/main/java/sprintB/us07/Vehicle.java
@@ -0,0 +1,127 @@
+package sprintB.us07;
+
+import java.util.Date;
+public class Vehicle {
+    private String brand;
+    private String model;
+    private String type;
+    private String plateID;
+    private double tareWeight;
+    private double grossWeight;
+    private double currentKM;
+    private Date registerDate;
+    private Date acquisitionDate;
+    private double maintenanceFrequency; // in km
+
+    //Constructor
+    public Vehicle(String brand, String model, String type, String plateID, double tareWeight, double grossWeight, double currentKM, Date registerDate, Date acquisitionDate, double maintenanceFrequency) {
+        this.brand = brand;
+        this.model = model;
+        this.type = type;
+        this.plateID = plateID;
+        this.tareWeight = tareWeight;
+        this.grossWeight = grossWeight;
+        this.currentKM = currentKM;
+        this.registerDate = registerDate;
+        this.acquisitionDate = acquisitionDate;
+        this.maintenanceFrequency = maintenanceFrequency;
+    }
+
+    //getter & setter methods
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPlateID() {
+        return plateID;
+    }
+
+    public void setPlateID(String plateID) {
+        this.plateID = plateID;
+    }
+
+    public double getTareWeight() {
+        return tareWeight;
+    }
+
+    public void setTareWeight(double tareWeight) {
+        this.tareWeight = tareWeight;
+    }
+
+    public double getGrossWeight() {
+        return grossWeight;
+    }
+
+    public void setGrossWeight(double grossWeight) {
+        this.grossWeight = grossWeight;
+    }
+
+    public double getCurrentKM() {
+        return currentKM;
+    }
+
+    public void setCurrentKM(double currentKM) {
+        this.currentKM = currentKM;
+    }
+
+    public Date getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(Date registerDate) {
+        this.registerDate = registerDate;
+    }
+
+    public Date getAcquisitionDate() {
+        return acquisitionDate;
+    }
+
+    public void setAcquisitionDate(Date acquisitionDate) {
+        this.acquisitionDate = acquisitionDate;
+    }
+
+    public double getMaintenanceFrequency() {
+        return maintenanceFrequency;
+    }
+
+    public void setMaintenanceFrequency(double maintenanceFrequency) {
+        this.maintenanceFrequency = maintenanceFrequency;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", type='" + type + '\'' +
+                ", plateID='" + plateID + '\'' +
+                ", tareWeight=" + tareWeight +
+                ", grossWeight=" + grossWeight +
+                ", currentKM=" + currentKM +
+                ", registerDate='" + registerDate + '\'' +
+                ", acquisitionDate='" + acquisitionDate + '\'' +
+                ", maintenanceFrequency=" + maintenanceFrequency +
+                '}';
+    }
+}
